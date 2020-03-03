1b8760764aca1be4b692416b1f2392f4592bdd23
Monadic Cat <greenmonadcat@gmail.com>
2020-03-03 09:43:03 -0600

    Add uglifyjs install

--- prep.sh
+++ prep.sh
@@ -2,12 +2,19 @@
 
 # Prep for build on Netlify.
 
+# Create directory for any binaries we fetch
+mkdir bin
+export PATH="$(pwd)/bin:$PATH"
+
 which elm
 if [ $? != 0 ] ; then
     curl -L -o elm.gz https://github.com/elm/compiler/releases/download/0.19.1/binary-for-linux-64-bit.gz
     gunzip elm.gz
     chmod +x elm
-    mkdir bin
     mv elm bin/elm
-    export PATH="$(pwd)/bin:$PATH"
+fi
+
+which uglifyjs
+if [ $? != 0 ] ; then
+    npm install -g uglify-js
 fi
